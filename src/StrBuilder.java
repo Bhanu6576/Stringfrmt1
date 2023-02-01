538f972a7e3f30bcd62ec28967a2723478b5c274
Bhanu6576 <judek9999@gmail.com>
2023-02-01 15:48:39 +0530

    Initial commit

---
+++ src/StrBuilder.java
@@ -0,0 +1,12 @@
+public class StrBuilder {
+    public static void main(String[] args) {
+        StringBuilder s1=new StringBuilder("Bhanu");
+        StringBuilder s2=new StringBuilder(" Prasad");
+        StringBuilder s=s1.append(s2);
+       // String a=s.toString();
+//        System.out.println(a);
+        System.out.println(s.toString());
+
+
+    }
+}
---
+++ src/StrFormat.java
@@ -0,0 +1,9 @@
+public class StrFormat {
+    public static void main(String[] args) {
+        String s1=new String("Bhanu");
+        String s2=new String("Prasad");
+        String s=String.format(s2);
+        System.out.println(s.toString());
+
+    }
+}
